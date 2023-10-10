a95eccb15fed0445e95a362a5e122caf1f498579
pashmash372 <abhilashsivasanker@gmail.com>
2023-10-11 01:17:30 +0530

    18. Understanding Spread & Rest Operators

--- javacript-tuts/play.js
+++ javacript-tuts/play.js
@@ -6,6 +6,11 @@ const person = {
     }
 };
 
+const copiedPerson = {...person};
+console.log(copiedPerson);
+// { name: 'John', age: 30, greet: [Function: greet] }
+
+
 person.greet();
 
 
@@ -21,3 +26,36 @@ console.log(hobbies);
 hobbies.push('Programming');
 
 console.log(hobbies); // hobbies is a constant but we can still push to it because we are not changing the pointer to the array, we are just changing the array itself
+
+const copiedArray = hobbies.slice(); // slice() copies the array
+console.log(copiedArray);   // copiedArray is a copy of hobbies but it is not the same array
+// [ 'Sports', 'Cooking', 'Programming' ]
+
+const copiedArray2 = [hobbies]; // this is not a copy of hobbies, it is an array inside an array
+
+console.log(copiedArray2); // this is not a copy of hobbies, it is an array inside an array
+// [ [ 'Sports', 'Cooking', 'Programming' ] ]
+
+const copiedArray3 = [...hobbies]; // this is a copy of hobbies
+console.log(copiedArray3); // this is a copy of hobbies 
+// [ 'Sports', 'Cooking', 'Programming' ]
+
+
+// spread operator works for objects too but it only copies the top level, it does not copy nested objects
+
+const toArray = (arg1, arg2, arg3) => {
+    return [arg1, arg2, arg3];
+}
+
+console.log(toArray(1, 2, 3)); // [ 1, 2, 3 ]
+
+const toArray1 = (...args) => { // ...args is the rest operator
+    return args;
+}
+
+
+console.log(toArray1(1, 2, 3, 4)); // [ 1, 2, 3, 4 ]
+
+// rest operator is used to merge a list of function arguments into an array
+
+// rest operator looks like spread operator but it is not, it is used in a different context   
