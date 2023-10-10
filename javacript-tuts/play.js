84772268522f77fdc0cf4769ed589feb1a71dae0
pashmash372 <abhilashsivasanker@gmail.com>
2023-10-10 15:04:44 +0530

    15. Working with Objects, Properties & Methods

--- javacript-tuts/play.js
+++ javacript-tuts/play.js
@@ -1,21 +1,9 @@
-const name = 'John Doe';
-let age = 25;
-let hasHobbies = true;
+const person = {
+    name: "John",
+    age: 30,
+    greet() {
+        console.log("Hi, I am " + this.name);
+    }
+};
 
-const summarizeUser =  (userName, userAge, userHasHobby) => {
-    return (
-        'Name is ' + userName +
-        ', age is ' + userAge +
-        ' and the user has hobbies: ' + userHasHobby
-    )
-}
-
-const add = (a, b) => {
-    return a + b;
-}
-
-const addOne = a => a + 1;
-
-console.log(add(1, 2));
-console.log(addOne(1));
-console.log(summarizeUser(name, age, hasHobbies));
+person.greet();
