0002141e979df32ca637601d6e92439fb17b6cab
pashmash372 <abhilashsivasanker@gmail.com>
2023-10-11 01:11:29 +0530

    17. Arrays, Objects & Reference Types

--- javacript-tuts/play.js
+++ javacript-tuts/play.js
@@ -17,3 +17,7 @@ for (let hobby of hobbies) {
 console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
 
 console.log(hobbies);
+
+hobbies.push('Programming');
+
+console.log(hobbies); // hobbies is a constant but we can still push to it because we are not changing the pointer to the array, we are just changing the array itself
