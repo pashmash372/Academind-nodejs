0cca27b431100c0356bbb7dfddbd05be6565a88c
pashmash372 <51986499+pashmash372@users.noreply.github.com>
2023-10-12 02:14:32 +0530

    66. Using Express Router

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -5,21 +5,12 @@ const express = require('express');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false})); // Register a middleware
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');    
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-}); 
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>'); // Sends a response   
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
