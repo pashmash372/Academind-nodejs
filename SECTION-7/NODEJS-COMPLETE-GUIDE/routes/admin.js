1dba0778d50402171047ec7f10f9887c255de18b
pashmash372 <51986499+pashmash372@users.noreply.github.com>
2023-10-15 14:11:06 +0530

    98. Adding Controllers
    skiped section 6 because of lot boring stuff there

---
+++ SECTION-7/NODEJS-COMPLETE-GUIDE/routes/admin.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+
+router.get('/add-product',productsController.getAddProduct);
+
+router.post('/add-product',productsController.postAddProduct);
+
+module.exports = router;
